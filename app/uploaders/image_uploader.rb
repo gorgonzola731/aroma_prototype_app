44a723ee9a79a17cccf5a6eb34daa8260a29ba29
gorgonzola731 <nk.kenken31@gmail.com>
2021-02-09 19:19:47 +0900

    画像投稿機能を実装

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,19 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def size_range
+    0..5.megabytes
+  end
+
+ 
+
+end
