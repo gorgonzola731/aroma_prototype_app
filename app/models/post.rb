7f4245044ecfd9752c40f245310166381b6df34c
gorgonzola731 <nk.kenken31@gmail.com>
2021-03-20 21:14:20 +0900

    一覧ページのレイアウト変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true,
-                   length: {maximum: 50}
+  validates :title, presence: true,length: {maximum: 50}
   belongs_to :user
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
